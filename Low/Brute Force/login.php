9c3ba77543b8b22c68b5769507704c8eba77db22
박성진 <sungjin1014@proton.me>
2025-12-12 09:43:02 +0900

    이번 Brute Force 로그인 기능으로 구현된 서버측 PHP 코드입니다

--- Low/Brute Force/login.php
+++ Low/Brute Force/login.php
@@ -8,7 +8,7 @@ if( isset( $_GET[ 'Login' ] ) ) {
     $pass = $_GET[ 'password' ];
     $pass = md5( $pass );
 
-    // Check the database
+    // Check database
     $query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
     $result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
 
